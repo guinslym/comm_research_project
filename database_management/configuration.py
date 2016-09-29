ec128335b1736562f6d072b75f84059a13a43c9c
Guinslym <agmond@gmx.com.br>
2016-09-29 17:56:48 -0400

    config folder

---
+++ database_management/configuration.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__='Guinsly Mondésir'
+__copyrights__=['University of Ottawa']
+__version__="1.0.0"
+__status__='Production'
+__license__='GPL'
+
+
+config = {
+
+}
